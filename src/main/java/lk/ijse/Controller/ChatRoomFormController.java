fadc2ff5b0e0052a9122a1e0d772afc9e88e690f
MinuraWije <minurawije@gmail.com>
2024-02-01 21:15:40 +0530

    Chat room ui done with controller sample skeleton.

--- src/main/java/lk/ijse/Controller/ChatRoomFormController.java
+++ src/main/java/lk/ijse/Controller/ChatRoomFormController.java
@@ -1,4 +1,38 @@
 package lk.ijse.Controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+import java.awt.event.MouseEvent;
+
 public class ChatRoomFormController {
+
+    @FXML
+    private Pane emojiPane;
+
+    @FXML
+    private JFXTextField txtMessage;
+
+    @FXML
+    void attachOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSendOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void emojiOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void txtMessageOnAction(ActionEvent event) {
+
+    }
+
 }
